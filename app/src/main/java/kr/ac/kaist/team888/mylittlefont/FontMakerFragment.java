c86e2ebcb7a694f8d970416fa423f240056f1905
JeOhLee <lott9@kaist.ac.kr>
2017-05-26 17:19:46 +0900

    [#48]Make FontMakerFragment gets text input from user

--- app/src/main/java/kr/ac/kaist/team888/mylittlefont/FontMakerFragment.java
+++ app/src/main/java/kr/ac/kaist/team888/mylittlefont/FontMakerFragment.java
@@ -1,20 +1,31 @@
 package kr.ac.kaist.team888.mylittlefont;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Switch;
 
 import kr.ac.kaist.team888.locator.Locator;
 import kr.ac.kaist.team888.util.FeatureController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FontMakerFragment extends Fragment {
   private FontCanvasView fontCanvasView;
+  private EditText sampleTextInput;
+  private String drawingText;
+  private ArrayList<Locator> locators;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +39,54 @@ public class FontMakerFragment extends Fragment {
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
+    drawingText = "";
+    locators = new ArrayList<>();
+
     fontCanvasView = (FontCanvasView) view.getRootView().findViewById(R.id.fontCanvas);
+    fontCanvasView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        InputMethodManager inputManager =
+            (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(sampleTextInput.getWindowToken(),
+            InputMethodManager.HIDE_NOT_ALWAYS);
+      }
+    });
+
+    sampleTextInput = (EditText) view.getRootView().findViewById(R.id.sample_text);
+    sampleTextInput = (EditText) view.getRootView().findViewById(R.id.sample_text);
+    sampleTextInput.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence sequence, int start, int before, int count) {
+
+      }
+
+      @Override
+      public void afterTextChanged(Editable sequence) {
+        HashMap<Character, Locator> charLocatorMap = new HashMap<>();
+        for (int i = 0; i < drawingText.length(); i++) {
+          charLocatorMap.put(drawingText.charAt(i), locators.get(i));
+        }
+
+        locators = new ArrayList<>();
+        for (int i = 0; i < sequence.length(); i++) {
+          char key = sequence.charAt(i);
+          if (charLocatorMap.containsKey(key)) {
+            locators.add(charLocatorMap.get(key));
+          } else {
+            locators.add(new Locator(key));
+          }
+        }
+
+        fontCanvasView.drawLocators(locators);
+        drawingText = sequence.toString();
+      }
+    });
 
     Switch viewSkeletonBtn = (Switch) view.getRootView().findViewById(R.id.viewSkeletonBtn);
     viewSkeletonBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
